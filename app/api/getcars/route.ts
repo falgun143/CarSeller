9688e738c6d10eba9c6576cb1874dd1c8a3cf545
falgun143 <palfalgun@gmail.com>
2024-08-03 22:39:42 +0530

    Removing Caching

--- app/api/getcars/route.ts
+++ app/api/getcars/route.ts
@@ -1,15 +1,20 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../lib/prisma";
 
-
 export async function GET(request: NextRequest) {
-    try{
-        const cars= await prisma.cars.findMany();
-       return NextResponse.json({cars,message:"Fetched Cars Successfully"},{status:200},)
-
-    }
-    catch(error){
-        return NextResponse.json({ message: 'Error while getting cars' }, { status: 500 });
-    }
- 
+  try {
+    const cars = await prisma.cars.findMany();
+    return NextResponse.json(
+      { cars, message: "Fetched Cars Successfully" },
+      {
+        status: 200,
+        headers: {
+          'Cache-Control': 'no-store', 
+        },
+      }
+    );
+  } catch (error) {
+    console.error("Error fetching cars:", error); 
+    return NextResponse.json({ message: 'Error while getting cars' }, { status: 500 });
+  }
 }
